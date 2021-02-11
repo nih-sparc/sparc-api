384bcab6b6e47f2c091db33341e7bf97e5f393c2
hsorby <h.sorby@auckland.ac.nz>
2021-02-12 11:08:30 +1300

    Fix PEP8 warnings in test_scicrunch.py.

--- tests/test_scicrunch.py
+++ tests/test_scicrunch.py
@@ -2,25 +2,30 @@ import json
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     # Spin up test flask app
     app.config['TESTING'] = True
     return app.test_client()
 
+
 def test_scicrunch_keys(client):
     r = client.get('/search/')
     assert r.status_code == 200
     assert 'numberOfHits' in json.loads(r.data).keys()
+
 
 def test_scicrunch_search(client):
     r = client.get('/search/heart')
     assert r.status_code == 200
     assert json.loads(r.data)['numberOfHits'] > 4
 
+
 def test_scicrunch_filter(client):
     r = client.get('/filter-search/', query_string={'term': 'genotype', 'facet': 'heart'})
     assert json.loads(r.data)['numberOfHits'] > 4
+
 
 def test_getting_facets(client):
     r = client.get('/get-facets/genotype')

c08052cf4adb0e3c4830b2bde7844b70eb5143c1
JeremyLecoeur <jeremy@blackfynn.com>
2021-04-19 14:35:04 -0400

    try removing test

--- tests/test_api.py
+++ tests/test_api.py
@@ -30,19 +30,6 @@ def test_direct_download_url_large_file(client):
     assert 'File too big to download' in r.get_data().decode()
 
 
-def test_get_owner_email(client):
-    # SPARC Portal user info
-    portal_user_id = 729
-    portal_user_email = 'nih-data-portal@blackfynn.com'
-
-    r = client.get(f"/get_owner_email/{portal_user_id}")
-    assert r.status_code == 200
-    assert r.get_json()['email'] == portal_user_email
-
-    r = client.get(f"/get_owner_email/{999999}")
-    assert r.status_code == 404
-
-    
 def test_get_datasets_by_project(client):
   # SPARC Portal project info
   portal_project_id = 'OT2OD025340'

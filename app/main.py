bd472bf375e719ffbb737890fa8118c963d5adef
Alan Garny <agarny@hellix.com>
2021-05-06 13:01:39 +1200

    Prevent the app from crashing if SPARC_PORTAL_AWS_KEY and/or SPARC_PORTAL_AWS_SECRET cannot be found.

--- app/main.py
+++ app/main.py
@@ -40,8 +40,11 @@ s3 = boto3.client(
     region_name="us-east-1",
 )
 
-os.environ["AWS_ACCESS_KEY_ID"] = Config.SPARC_PORTAL_AWS_KEY
-os.environ["AWS_SECRET_ACCESS_KEY"] = Config.SPARC_PORTAL_AWS_SECRET
+try:
+    os.environ["AWS_ACCESS_KEY_ID"] = Config.SPARC_PORTAL_AWS_KEY
+    os.environ["AWS_SECRET_ACCESS_KEY"] = Config.SPARC_PORTAL_AWS_SECRET
+except:
+    pass
 
 biolucida_lock = Lock()
 

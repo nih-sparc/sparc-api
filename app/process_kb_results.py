3aaaea8a4ab00800424125000ec57519d82565e4
Alan Garny <agarny@hellix.com>
2021-06-25 10:09:29 +1200

    Improved way of filtering scaffold datasets.

--- app/process_kb_results.py
+++ app/process_kb_results.py
@@ -126,7 +126,7 @@ def facet_query_string(query, terms, facets, type_map):
                 qt += "("
             for l in t[k]:
                 if l == "scaffolds":
-                    qt += "scaffolds.object.mimetype.name:((inode%2fvnd.abi.scaffold+directory))"
+                    qt += "objects.additional_mimetype.name:((inode%2fvnd.abi.scaffold) AND (file))"
                 elif l == "simulations":
                     qt += "xrefs.additionalLinks.description:((CellML) OR (SED-ML))"
                 if l is not t[k][-1]:

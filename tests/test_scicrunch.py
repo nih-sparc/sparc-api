437af41400762152e480c92c9162c3f947b33ab7
Jesse Khorasanee <jessekhorasanee@gmail.com>
2021-12-10 13:04:57 +1300

    Fix test dois

--- tests/test_scicrunch.py
+++ tests/test_scicrunch.py
@@ -53,19 +53,18 @@ def test_scicrunch_dataset_doi(client):
         pytest.skip('DOI used in test is out of date.')
 
 def test_scicrunch_multiple_dataset_doi(client):
-    # Testing with dataset 55
-    identifier = "55"
-    run_doi_test_1 = check_doi_status(client, identifier, '10.26275/pzek-91wx')
-    run_doi_test_2 = check_doi_status(client, "139", '10.26275/pzek-91wx')
+    # Testing with dataset 55 and 115
+    run_doi_test_1 = check_doi_status(client, "55", '10.26275/pzek-91wx')
+    run_doi_test_2 = check_doi_status(client, "68", '10.26275/4qvr-kwzq')
 
     if run_doi_test_1 and run_doi_test_2:
-        r = client.get('/dataset_info/using_multiple_dois/?dois=10.26275%2Fngey-3iz7&dois=10.26275%2F63lh-hdz5')
-        dataset_version = json.loads(r.data)['hits']['hits'][0]['_source']['item']['version']['keyword']
+        r = client.get('/dataset_info/using_multiple_dois/?dois=10.26275%2Fpzek-91wx&dois=10.26275%2F4qvr-kwzq')
+        dataset_version = json.loads(r.data)['results'][0]['version']
         if version.parse(dataset_version) >= version.parse("1.1.4"):
-            discover_id_1 = json.loads(r.data)['hits']['hits'][0]['_id']
-            discover_id_2 = json.loads(r.data)['hits']['hits'][1]['_id']
-            assert discover_id_1 == "55" or discover_id_1 == "139"
-            assert discover_id_2 == "55" or discover_id_2 == "139"
+            discover_id_1 = json.loads(r.data)['results'][0]['dataset_identifier']
+            discover_id_2 = json.loads(r.data)['results'][1]['dataset_identifier']
+            assert discover_id_1 == "55" or discover_id_1 == "68"
+            assert discover_id_2 == "55" or discover_id_2 == "68"
     else:
         pytest.skip('DOI used in test is out of date.')
 

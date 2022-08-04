ba0d4e5e0be6136573496258bf23ede2991e8521
alan-wu <alan.wu@auckland.ac.nz>
2022-08-04 16:17:28 +1200

    Remove repeated test.

--- tests/test_dataset_info.py
+++ tests/test_dataset_info.py
@@ -142,19 +142,6 @@ def test_generic_mouse_colon_dataset_search(client):
     assert SCAFFOLD_FILE in result.keys()
     assert THUMBNAIL_IMAGE in result.keys()
 
-def test_physiological_data_visualisation_search(client):
-    #title = "Test case for physiological data visualisation"
-    r = client.get('/dataset_info/using_pennsieve_identifier', query_string={'identifier': 141})
-    response = json.loads(r.data)
-    assert 'result' in response
-    assert len(response['result']) == 1
-    result = response['result'][0]
-    # print(result)
-    assert "Test case for physiological data visualisation" == result['name']
-    assert PLOT_FILE in result.keys()
-    #'text/vnd.abi.plot+tab-separated-values' and #'text/vnd.abi.plot+csv'
-    assert len(result[PLOT_FILE]) == 21
-
 def test_complex_title_dataset_search(client):
     title = "Spatial distribution and morphometric characterization of vagal efferents associated with the myenteric plexus of the rat stomach"
 

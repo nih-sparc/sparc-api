a9452e1c50dc75971b949e0f090552b8791413d0
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2022-11-02 10:33:58 +0100

    revert commit

---
+++ app/metrics/algolia.py
@@ -0,0 +1,16 @@
+from algoliasearch.search_client import SearchClient
+from app.config import Config
+
+APP_ID = Config.ALGOLIA_APP_ID
+API_KEY = Config.ALGOLIA_API_KEY
+INDEX = Config.ALGOLIA_API_INDEX
+
+
+def init_algolia_client():
+    return SearchClient.create(APP_ID, API_KEY)
+
+
+def get_dataset_count(client):
+    index = client.init_index(INDEX)
+    res = index.search("")
+    return res["nbHits"]
---
+++ app/metrics/ga.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from app.config import Config
+from dateutil.relativedelta import relativedelta
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+SCOPES = [Config.GOOGLE_API_GA_SCOPE]
+KEY_PATH = Config.GOOGLE_API_GA_KEY_PATH
+VIEW_ID = Config.GOOGLE_API_GA_VIEW_ID
+
+
+def init_ga_reporting():
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        KEY_PATH,
+        SCOPES
+    )
+    analytics = build('analyticsreporting', 'v4', credentials=credentials)
+    return analytics
+
+
+def get_ga_1year_sessions(analytics):
+
+    start_date = datetime.now() - relativedelta(years=1)
+    formatted_start_date = start_date.strftime('%Y-%m-%d')
+
+    try:
+        report = analytics.reports().batchGet(
+            body={
+                "reportRequests": [{
+                    "viewId": VIEW_ID,
+                    "dateRanges": [{
+                        "startDate": formatted_start_date,
+                        "endDate": datetime.now().strftime('%Y-%m-%d')
+                    }],
+                    "metrics": [{"expression": "ga:sessions"}]
+                }]
+            }
+        ).execute()
+
+        if len(report["reports"]):
+            total = report["reports"][0]["data"]["totals"][0]["values"][0]
+            return int(total)
+
+    except:
+        return None

b903de9fca5e7869d8f5ea6883b60d15858527b4
Eric Gauzens <ericgauzens@hotmail.com>
2023-02-05 19:21:04 -0500

    updated sort order

--- scripts/update_contentful_entries.py
+++ scripts/update_contentful_entries.py
@@ -12,16 +12,17 @@ def update_event_entries():
         start_date = fields_dict['start_date']
         #convert from ISO time format provided by contentful in UTC timezone to naive offset datetime object
         start_date_datetime = datetime.strptime(datetime.fromisoformat(start_date).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f'), '%Y-%m-%d %H:%M:%S.%f')
-        timeFromEventInSeconds = (start_date_datetime - now).total_seconds()
-        timeFromEventInDays = timeFromEventInSeconds / 86400
+        time_from_event_in_seconds = (start_date_datetime - now).total_seconds()
+        time_from_event_in_days = time_from_event_in_seconds / 86400
+        # in order to maintain the correct event sorting for upcoming (closet first, followed by closest in the future, followed by closest in the past),
+        # we cannot simply keep track of the time from the event. Instead we take the inverse of the dates in the future so that they are less than the nearest future dates.
+        upcoming_sort_order = 1
+        if time_from_event_in_days > 0:
+          upcoming_sort_order = 1/time_from_event_in_days
+        if time_from_event_in_days < 0:
+          upcoming_sort_order = time_from_event_in_days
         version = sys_dict['version']
         if 'Test Event' in title:
-          entry.title = 'Updated Title'
+          entry.upcoming_sort_order = upcoming_sort_order
           entry.save()
-
-        #start_date = entry.fields()['start_date']
-        #version = entry.sys
-        #print("For {}: Start date = {}, Version = {}", title, start_date, version)
-        print("Title = ", title)
-        print("Start Date = ", start_date)
-        print("Time from event = ", timeFromEventInDays)
+          

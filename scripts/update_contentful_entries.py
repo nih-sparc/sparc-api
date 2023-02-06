45f4a1cd7a41a0dcc30886633bd24652ff6b390c
Eric Gauzens <ericgauzens@hotmail.com>
2023-02-05 19:22:59 -0500

    Finished updating entries

--- scripts/update_contentful_entries.py
+++ scripts/update_contentful_entries.py
@@ -6,9 +6,7 @@ def update_event_entries():
     now = datetime.now()
     for entry in all_event_entries:
       fields_dict = entry.fields()
-      sys_dict = entry.sys
-      if 'title' in fields_dict and 'start_date' in fields_dict and 'version' in sys_dict:
-        title = fields_dict['title']
+      if 'title' in fields_dict and 'start_date' in fields_dict:
         start_date = fields_dict['start_date']
         #convert from ISO time format provided by contentful in UTC timezone to naive offset datetime object
         start_date_datetime = datetime.strptime(datetime.fromisoformat(start_date).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f'), '%Y-%m-%d %H:%M:%S.%f')
@@ -21,8 +19,5 @@ def update_event_entries():
           upcoming_sort_order = 1/time_from_event_in_days
         if time_from_event_in_days < 0:
           upcoming_sort_order = time_from_event_in_days
-        version = sys_dict['version']
-        if 'Test Event' in title:
-          entry.upcoming_sort_order = upcoming_sort_order
-          entry.save()
-          
+        entry.upcoming_sort_order = upcoming_sort_order
+        entry.save()

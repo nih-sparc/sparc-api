978ab4a239c005f68e26718a004f56ed1cac00cc
Eric Gauzens <ericgauzens@hotmail.com>
2023-02-05 20:42:00 -0500

    cleanup

--- scripts/update_contentful_entries.py
+++ scripts/update_contentful_entries.py
@@ -2,32 +2,30 @@ from app.metrics.contentful import get_all_entries
 from datetime import datetime, timezone
 
 def update_event_entries():
-    print("UPDATE RAN!!!!!!!!!!!")
     all_event_entries = get_all_entries("event")
     now = datetime.now()
     need_to_update = False
     for entry in all_event_entries:
-      fields_dict = entry.fields()
-      if 'start_date' in fields_dict and 'upcoming_sort_order' in fields_dict:
-        # This means a new event was added so we must update the upcoming sort order
-        if fields_dict['upcoming_sort_order'] == 0:
-          need_to_update = True
-    if (need_to_update):
-      for entry in all_event_entries:
         fields_dict = entry.fields()
         if 'start_date' in fields_dict and 'upcoming_sort_order' in fields_dict:
-          start_date = fields_dict['start_date']
-          #convert from ISO time format provided by contentful in UTC timezone to naive offset datetime object
-          start_date_datetime = datetime.strptime(datetime.fromisoformat(start_date).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f'), '%Y-%m-%d %H:%M:%S.%f')
-          time_from_event_in_seconds = (start_date_datetime - now).total_seconds()
-          time_from_event_in_days = time_from_event_in_seconds / 86400
-          # in order to maintain the correct event sorting for upcoming (closet first, followed by closest in the future, followed by closest in the past),
-          # we cannot simply keep track of the time from the event. Instead we take the inverse of the dates in the future so that they are less than the nearest future dates.
-          upcoming_sort_order = 1
-          if time_from_event_in_days > 0:
-            upcoming_sort_order = 1/time_from_event_in_days
-          if time_from_event_in_days < 0:
-            upcoming_sort_order = time_from_event_in_days
-          entry.upcoming_sort_order = upcoming_sort_order
-          entry.save()
-
+            # This means a new event was added so we must update the upcoming sort order
+            if fields_dict['upcoming_sort_order'] == 0:
+              need_to_update = True
+    if (need_to_update):
+        for entry in all_event_entries:
+            fields_dict = entry.fields()
+            if 'start_date' in fields_dict and 'upcoming_sort_order' in fields_dict:
+                start_date = fields_dict['start_date']
+                #convert from ISO time format provided by contentful in UTC timezone to naive offset datetime object
+                start_date_datetime = datetime.strptime(datetime.fromisoformat(start_date).astimezone(timezone.utc).strftime('%Y-%m-%d %H:%M:%S.%f'), '%Y-%m-%d %H:%M:%S.%f')
+                time_from_event_in_seconds = (start_date_datetime - now).total_seconds()
+                time_from_event_in_days = time_from_event_in_seconds / 86400
+                # in order to maintain the correct event sorting for upcoming (closet first, followed by closest in the future, followed by closest in the past),
+                # we cannot simply keep track of the time from the event. Instead we take the inverse of the dates in the future so that they are less than the nearest future dates.
+                upcoming_sort_order = 1
+                if time_from_event_in_days > 0:
+                    upcoming_sort_order = 1/time_from_event_in_days
+                if time_from_event_in_days < 0:
+                    upcoming_sort_order = time_from_event_in_days
+                entry.upcoming_sort_order = upcoming_sort_order
+                entry.save()

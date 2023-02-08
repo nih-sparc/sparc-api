3914448d8be1e82c00d0dadc28031c21b33b2791
Eric Gauzens <ericgauzens@hotmail.com>
2023-02-08 10:20:31 -0500

    added schema

--- app/metrics/contentful.py
+++ app/metrics/contentful.py
@@ -52,7 +52,7 @@ def get_all_entries(content_type_id):
   return content_type.entries().all()
 
 def get_all_published_entries(content_type_id):
-    url = f'{Config.CTF_CMA_API_HOST}/spaces/{Config.CTF_SPACE_ID}/environments/master/public/entries?access_token={Config.CTF_CMA_ACCESS_TOKEN}&content_type={content_type_id}'
+    url = f'https://{Config.CTF_CMA_API_HOST}/spaces/{Config.CTF_SPACE_ID}/environments/master/public/entries?access_token={Config.CTF_CMA_ACCESS_TOKEN}&content_type={content_type_id}'
     response = requests.get(url)
     print("RESPONSE = ", response)
     return response.items

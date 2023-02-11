0ce182132634712baae5a447d4020665df41118a
Eric Gauzens <ericgauzens@hotmail.com>
2023-02-11 16:27:28 -0500

    bug fix

--- app/main.py
+++ app/main.py
@@ -768,7 +768,7 @@ def datasets_by_project_id(project_id):
 
 @app.route("/get_featured_dataset", methods=["GET"])
 def get_featured_dataset():
-    featured_dataset_id = get_featured_dataset_id_table_state()["featured_dataset_id"]
+    featured_dataset_id = get_featured_dataset_id_table_state(featuredDatasetIdSelectorTable)["featured_dataset_id"]
     if featured_dataset_id == -1:
         # In case there was an error while setting the id, just return a default dataset so the homepage does not break
         default_id = 32

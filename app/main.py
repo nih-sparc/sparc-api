6ad65b1cbe9dc5f80ec49028dc1590be268d8599
Eric Gauzens <ericgauzens@hotmail.com>
2023-02-11 15:32:36 -0500

    debug

--- app/main.py
+++ app/main.py
@@ -240,6 +240,7 @@ def set_featured_dataset_id():
         homepage_cma_published_entry = get_cma_published_entry(Config.CTF_HOMEPAGE_ID)
         if 'dateToClearFeaturedDatasets' in homepage_cma_published_entry['fields']:
           date_to_clear_datasets = homepage_cma_published_entry['fields']['dateToClearFeaturedDatasets']['en-US']
+          print("DATE = ", date_to_clear_datasets)
           if (date_to_clear_datasets - datetime.now()).total_seconds() <= 0:
               # Clear featured datasets/date and re-publish homepage
               if 'featuredDatasets' in homepage_cma_published_entry['fields']:

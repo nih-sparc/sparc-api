da2d9fe133e879ef3f943780f19ba1a71ab2bb13
Eric Gauzens <ericgauzens@hotmail.com>
2023-02-28 08:12:02 -0500

    Updated contentful events webhook

--- app/main.py
+++ app/main.py
@@ -1311,12 +1311,13 @@ def search_readme(query):
 def metrics():
     return usage_metrics
 
-# Callback endpoint for contentful event created webhook
-@app.route("/event_created", methods=["POST"])
-def event_created():
+# Callback endpoint for contentful event updated webhook that gets triggered when an event is updated in Contentful
+@app.route("/event_updated", methods=["POST"])
+def event_updated():
     # the webhook secret key is configured with the same value as the cda access token. If the cda access token is updated then we must update this as well.
-    if request.headers.get('event_created_secret_key') != Config.CTF_CDA_ACCESS_TOKEN:
-        abort(403, description="Invalid secret key")
+    secret_key = request.headers.get('event_updated_secret_key')
+    if secret_key != Config.CTF_CDA_ACCESS_TOKEN:
+        abort(403, description=f'Invalid secret key: {secret_key}')
     else:
         event = request.get_json()
         if event:

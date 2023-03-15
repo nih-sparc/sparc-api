a8dd5c0cda7ca281f981190e1b1d11e83d3b01c7
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-03-15 16:15:42 +1300

    Make app/metrics/contentful.py mor PEP8 compliant.

--- app/metrics/contentful.py
+++ app/metrics/contentful.py
@@ -4,7 +4,7 @@ import contentful
 import contentful_management
 import requests
 
-#CDA is used for reading content, while CMA is used for updating content
+# CDA is used for reading content, while CMA is used for updating content
 CDA_API_HOST = Config.CTF_CDA_API_HOST
 CMA_API_HOST = Config.CTF_CMA_API_HOST
 CDA_ACCESS_TOKEN = Config.CTF_CDA_ACCESS_TOKEN
@@ -20,10 +20,11 @@ def init_cf_cda_client():
             api_url=CDA_API_HOST
         )
         return client
-        
+
     except Exception as e:
         logging.error('An error occured while instantiating the Contentful CDA client.', e)
         return None
+
 
 def init_cf_cma_client():
     try:
@@ -31,10 +32,11 @@ def init_cf_cma_client():
             CMA_ACCESS_TOKEN
         )
         return client
-        
+
     except Exception as e:
         logging.error('An error occured while instantiating the Contentful CMA client.', e)
         return None
+
 
 def get_funded_projects_count(client):
     response = client.entries({
@@ -42,29 +44,35 @@ def get_funded_projects_count(client):
     })
     return response.total
 
+
 def get_cda_client_entry(id):
-  client = init_cf_cda_client()
-  return client.entry(id)
+    client = init_cf_cda_client()
+    return client.entry(id)
+
 
 def get_all_entries(content_type_id):
     url = f'https://{Config.CTF_CMA_API_HOST}/spaces/{Config.CTF_SPACE_ID}/environments/master/entries?access_token={Config.CTF_CMA_ACCESS_TOKEN}&content_type={content_type_id}'
     response = requests.get(url=url)
     return response.json()['items']
 
+
 def get_all_published_entries(content_type_id):
     url = f'https://{Config.CTF_CMA_API_HOST}/spaces/{Config.CTF_SPACE_ID}/environments/master/public/entries?access_token={Config.CTF_CMA_ACCESS_TOKEN}&content_type={content_type_id}'
     response = requests.get(url)
     return response.json()['items']
+
 
 def get_cma_published_entry(id):
     url = f'https://{Config.CTF_CMA_API_HOST}/spaces/{Config.CTF_SPACE_ID}/environments/master/public/entries/{id}?access_token={Config.CTF_CMA_ACCESS_TOKEN}'
     response = requests.get(url)
     return response.json()
 
+
 def get_cma_entry(id):
     url = f'https://{Config.CTF_CMA_API_HOST}/spaces/{Config.CTF_SPACE_ID}/environments/master/entries/{id}?access_token={Config.CTF_CMA_ACCESS_TOKEN}'
     response = requests.get(url=url)
     return response.json()
+
 
 # Since get_all_published_entries has to use direct HTTP endpoint its response is in a different format than when using the client to get_all_entries
 # Therefore, in order to update an entry with that kind of response we must use this method instead of the client SDK update method
@@ -79,7 +87,7 @@ def update_entry_using_json_response(content_type, id, data):
         'X-Contentful-Content-Type': str(content_type),
         'X-Contentful-Version': str(version)
     }
-    
+
     response = requests.put(
         headers=hed,
         url=url,
@@ -87,13 +95,14 @@ def update_entry_using_json_response(content_type, id, data):
     )
     return response.json()
 
+
 def publish_entry(id, version):
     url = f'https://{Config.CTF_CMA_API_HOST}/spaces/{Config.CTF_SPACE_ID}/environments/master/entries/{id}/published'
     hed = {
         'Authorization': 'Bearer ' + Config.CTF_CMA_ACCESS_TOKEN,
         'X-Contentful-Version': str(version)
     }
-    
+
     response = requests.put(
         headers=hed,
         url=url

ee4d4863fb6f0f78f8ccb633811f40a402a75bc1
Ignacio Pascual <4764217+ignapas@users.noreply.github.com>
2023-03-22 15:04:04 +0100

    adapted test data to specs

--- app/osparc/services.py
+++ app/osparc/services.py
@@ -1,59 +1,67 @@
 services = [
     {
-        "id": 1,
-        "title": "Service 1",
-        "description": "This is the first service that has a very nice description",
-        "linkURL": "https://osparc.io/service/1",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "name_key": "simcore/services/dynamic/sim4life",
+        "name": "Sim4Life",
+        "description": "This is the first service that has a very nice description. And it is also sim4life for the web",
+        "view_url": "https://osparc.io/service/2",
+        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx",
+        "file_extensions": ["smash", "h5"]
     },
     {
-        "id": 12,
-        "title": "Service 2",
+        "name_key": "simcore/services/dynamic/rawg",
+        "name": "Service 2",
         "description": "This is the first service that has a very nice description",
-        "linkURL": "https://osparc.io/service/2",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "view_url": "https://osparc.io/service/2",
+        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx",
+        "file_extensions": ["csv"]
     },
     {
-        "id": 13,
-        "title": "Service 3",
+        "name_key": "simcore/services/dynamic/aservice",
+        "name": "Service 3",
         "description": "This is the first service that has a very nice description",
-        "linkURL": "https://osparc.io/service/3",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "view_url": "https://osparc.io/service/3",
+        "thumbnail": "https://m.media-amazon.com/images/M/MV5BN2EyYTkwNjUtY2I5MS00ZTFmLWI2ZGUtODBhMDgzZjk1YjgwXkEyXkFqcGdeQXVyNDc2NjEyMw@@._V1_.jpg",
+        "file_extensions": ["csv", "xlsx"]
     },
     {
-        "id": 14,
-        "title": "Service 4",
+        "name_key": "simcore/services/dynamic/sim2life",
+        "name": "Service 4",
         "description": "This is the first service that has a very nice description crocodile",
-        "linkURL": "https://osparc.io/service/4",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "view_url": "https://osparc.io/service/4",
+        "thumbnail": "https://www.longrunexploration.com/upload/main_banner/2/05/banner.jpg",
+        "file_extensions": ["smash", "jpg"]
     },
     {
-        "id": 15,
-        "title": "Service 5",
+        "name_key": "simcore/services/dynamic/bobo",
+        "name": "Service 5",
         "description": "This is the first service that has a very nice description",
-        "linkURL": "https://osparc.io/service/5",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "view_url": "https://osparc.io/service/5",
+        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx",
+        "file_extensions": ["jpg", "tif"]
     },
     {
-        "id": 16,
-        "title": "Service 7",
+        "name_key": "simcore/services/dynamic/crocoservice",
+        "name": "Service 7",
         "description": "This is the first service that has a very nice description",
-        "linkURL": "https://osparc.io/service/7",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "view_url": "https://osparc.io/service/7",
+        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx",
+        "file_extensions": ["croc"]
     },
     {
-        "id": 17,
-        "title": "Service CrocodilE",
+        "name_key": "simcore/services/dynamic/si",
+        "name": "Service CrocodilE",
         "description": "This is the first service that has a very nice description",
-        "linkURL": "https://osparc.io/service/8",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "view_url": "https://osparc.io/service/8",
+        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx",
+        "file_extensions": ["*"]
     },
     {
-        "id": 18,
-        "title": "Service 9",
+        "name_key": "simcore/services/dynamic/no",
+        "name": "Service 9",
         "description": "This is the first service that has a very nice description",
-        "linkURL": "https://osparc.io/service/9",
-        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx"
+        "view_url": "https://osparc.io/service/9",
+        "thumbnail": "https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-anatomy-teaser.ashx",
+        "file_extensions": ["croc", "jpg", "png"]
     },
 ]
 
@@ -65,7 +73,7 @@ def search_services(search_terms):
         norm_terms = search_terms.lower()
 
         def filter_fn(service):
-            if norm_terms in service.get('title').lower():
+            if norm_terms in service.get('name').lower():
                 return True
             if norm_terms in service.get('description').lower():
                 return True

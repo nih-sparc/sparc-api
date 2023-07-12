59b387351992c485917f18e82075546bb8106696
Eric Gauzens <ericgauzens@hotmail.com>
2023-07-12 13:48:55 -0400

    added logging

--- tests/test_update_contentful_entries.py
+++ tests/test_update_contentful_entries.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone, timedelta
 from scripts.update_contentful_entries import calculate_sort_order
 
 def test_calculate_sort_order():
-    now = datetime.now()    
+    now = datetime.now().astimezone(timezone.utc)
     # Format the date in ISO format
     current_date_iso = now.isoformat()
     # Subtract one day to get a day in the past
@@ -11,8 +11,8 @@ def test_calculate_sort_order():
     # Add one day to get a day in the future
     future_date = now + timedelta(days=1)
     future_date_iso = future_date.isoformat()
-
-    print(f"CURRENT_DATE = {current_date_iso}")
+    print(f"NOW UTC = ", now)
+    print(f"NOW ISO = {current_date_iso}")
     print(f"PAST_DATE = {past_date_iso}")
     print(f"FUTURE_DATE = {future_date_iso}")
 

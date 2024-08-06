e066a5428512dca859ebaffe7a92b851c4acd5c8
alan-wu <alan.wu@auckland.ac.nz>
2024-08-06 12:07:30 +1200

    Remove hardcoded email address.

--- app/config.py
+++ app/config.py
@@ -22,7 +22,7 @@ class Config(object):
     SES_ARN = os.environ.get("SES_ARN")
     SES_SENDER = os.environ.get("SES_SENDER")
     METRICS_EMAIL_ADDRESS = os.environ.get("METRICS_EMAIL_ADDRESS", "metrics@sparc.science")
-    TESTING_EMAIL_ADDRESS = os.environ.get("TESTING_EMAIL_ADDRESS", "jessekhora@gmail.com")
+    TESTING_EMAIL_ADDRESS = os.environ.get("TESTING_EMAIL_ADDRESS")
     COMMS_EMAIL = os.environ.get("COMMS_EMAIL")
     SPARC_PORTAL_AWS_KEY = os.environ.get("SPARC_PORTAL_USER_ID")
     SPARC_PORTAL_AWS_SECRET = os.environ.get("SPARC_PORTAL_USER_SECRET")
